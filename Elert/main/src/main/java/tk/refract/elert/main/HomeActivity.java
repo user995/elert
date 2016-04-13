14a7ce070a5d6e364760e0db78c1859a1b0f06a2
user995 <user995@users.noreply.github.com>
2016-04-13 18:53:31 +0200

    Added files via upload

---
+++ Elert/main/src/main/java/tk/refract/elert/main/HomeActivity.java
@@ -0,0 +1,15 @@
+package tk.refract.elert.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
