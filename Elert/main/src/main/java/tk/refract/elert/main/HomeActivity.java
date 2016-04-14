0643aae10f927c9272cbbef7c021e627330bdada
James Filmer <james.filmer@nmmu.ac.za>
2016-04-14 15:15:57 +0200

    update

--- Elert/main/src/main/java/tk/refract/elert/main/HomeActivity.java
+++ Elert/main/src/main/java/tk/refract/elert/main/HomeActivity.java
@@ -2,8 +2,6 @@ package tk.refract.elert.main;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
