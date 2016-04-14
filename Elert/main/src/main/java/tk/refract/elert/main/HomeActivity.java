11e923347a6e352cf366c5534ce91b7d1afd2f83
James Filmer <james.filmer@nmmu.ac.za>
2016-04-15 01:36:32 +0200

    14/04/2016 update

--- Elert/main/src/main/java/tk/refract/elert/main/HomeActivity.java
+++ Elert/main/src/main/java/tk/refract/elert/main/HomeActivity.java
@@ -1,37 +1,150 @@
 package tk.refract.elert.main;
 
-import android.app.Activity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.ViewAnimationUtils;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.gc.materialdesign.views.ButtonFloat;
 
-public class HomeActivity extends Activity {
-    private ListView mDrawerList;
-    private ArrayAdapter<String> mAdapter;
+public class HomeActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private RelativeLayout welcome;
+
+    private SharedPreferences sharedPref;
+
+    private Toolbar toolbar;
+    private NavigationView navView;
+    private String cell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //check preferences and see open main_home activity screen or 'register screen'
         setContentView(R.layout.activity_home);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        navView = (NavigationView) findViewById(R.id.navigation_view);
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        mDrawerList = (ListView)findViewById(R.id.navList);
-        addDrawerItems();
-
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(HomeActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if (menuItem.isChecked()) menuItem.setChecked(false);
+                else menuItem.setChecked(true);
+
+                //Closing drawer on item click
+                drawerLayout.closeDrawers();
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    case R.id.reset:
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.remove("name");
+                        editor.remove("cell");
+                        editor.commit();
+                        return true;
+                    default:
+                        return false;
+                }
             }
         });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        welcome = (RelativeLayout) findViewById(R.id.welcome);
+
+        String testCell = sharedPref.getString("cell", "null");
+        if (testCell.equals("null")) {
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            final ButtonFloat buttonFloat = (ButtonFloat) findViewById(R.id.buttonFloat);
+            welcome.setVisibility(View.VISIBLE);
+            buttonFloat.setVisibility(View.VISIBLE);
+            buttonFloat.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    welcomeInvisible();
+                    buttonFloat.setVisibility(View.GONE);
+
+
+                    EditText etName = (EditText) findViewById(R.id.etName);
+                    EditText etCell = (EditText) findViewById(R.id.etCell);
+                    String name = etName.getText().toString().trim();
+                    cell = etCell.getText().toString().trim();
+
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString("name", name);
+                    editor.putString("cell", cell);
+                    editor.commit();
+
+                    drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+                    toolbar.setVisibility(View.VISIBLE);
+                    ((TextView) findViewById(R.id.tv2Name)).setText(name);
+                    ((TextView) findViewById(R.id.tv2Phone)).setText(cell);
+                }
+            });
+        } else {
+            cell = testCell;
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+            toolbar.setVisibility(View.VISIBLE);
+            ((TextView) findViewById(R.id.tv2Name)).setText(sharedPref.getString("name", "error"));
+            ((TextView) findViewById(R.id.tv2Phone)).setText(cell);
+        }
+
+
     }
 
-    private void addDrawerItems() {
-        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
-        mDrawerList.setAdapter(mAdapter);
+
+    void welcomeVisible() {
+        int cx = welcome.getRight();
+        int cy = welcome.getBottom();
+        int finalRadius = Math.max(welcome.getWidth(), welcome.getHeight());
+        Animator anim = ViewAnimationUtils.createCircularReveal(welcome, cx, cy, 0, finalRadius);
+        welcome.setVisibility(View.VISIBLE);
+        anim.start();
+    }
+
+    void welcomeInvisible() {
+        int cx = welcome.getRight();
+        int cy = welcome.getBottom();
+        int initialRadius = Math.max(welcome.getWidth(), welcome.getHeight());
+        Animator anim = ViewAnimationUtils.createCircularReveal(welcome, cx, cy, initialRadius, 0);
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                super.onAnimationEnd(animator);
+                welcome.setVisibility(View.INVISIBLE);
+            }
+
+        });
+        anim.start();
     }
 }
